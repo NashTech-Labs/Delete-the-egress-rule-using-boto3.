3738c6ac26815161687c9a8d2147eca189a48aba
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 02:03:50 +0530

    Update in code

--- delete_egress_rule.py
+++ delete_egress_rule.py
@@ -1,3 +1,5 @@
+#By MuZakkir Saifi
+
 # import logging for get the logs in  execution
 import logging
 # import the boto3 which will use to interact  with the aws
@@ -5,36 +7,36 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the AWS REGION")
 
 # this is the configration for the logger
 
-logger = logging.getLogger()
+logger_for = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+vpc_client = boto3.client("ec2", region_name=REGION)
 
 
-def del_egress_rule(security_group_id, security_group_rule_ids):
+def del_rule(grp_id, grp_rule_ids):
  
     try:
-        response = vpc_client.revoke_security_group_egress(
-            GroupId=security_group_id,
-            SecurityGroupRuleIds=security_group_rule_ids)
+        res = vpc_client.revoke_security_group_egress(
+            GroupId=grp_id,
+            SecurityGroupRuleIds=grp_rule_ids)
 
     except ClientError:
-        logger.exception('Egress security group rule can not be deleted.')
+        logger_for.exception('Egress security group rule can not be deleted.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    SECURITY_GROUP_ID = input("Please enter the security group id")
-    SECURITY_GROUP_RULE_ID = input("Please enter the security group rule id")
-    logger.info(f'Please wait, We are removing a security group egress rule(s)...')
-    rule = del_egress_rule(SECURITY_GROUP_ID, SECURITY_GROUP_RULE_ID)
-    logger.info(
+    GRP_ID = input("Please enter the security group id")
+    SECURITY_GROUP_RULE_ID =['sgr-0f7468cfded']
+    logger_for.info(f'Please wait, We are removing a security group egress rule(s)...')
+    rule = del_rule(GRP_ID, SECURITY_GROUP_RULE_ID)
+    logger_for.info(
         f'Wow, Your security group egress rule(s) has been deleted: \n{json.dumps(rule, indent=4)}'
     )
